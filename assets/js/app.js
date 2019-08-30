12edf6a5ae78e2a2791963289f1c7e3185f28a9f
Ilya Ivanouski <domovikx@gmail.com>
2019-08-31 01:16:29 +0300

    feat: add Smooth scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,25 @@ $(() => {
     const header = $('#header')
     const intro = $('#intro')
 
+    /** header--fixed */
     $(window).on('scroll load resize', () => {
         const introH = intro.innerHeight()
-        const scrollTop = $(window).scrollTop()
+        const scrollTop = $(this).scrollTop()
 
         if (scrollTop > introH) {
             header.addClass('header--fixed')
         } else {
             header.removeClass('header--fixed')
         }
+    })
+
+    /** Smooth scroll*/
+    $('[data-scroll]').on('click', function (event) {
+        event.preventDefault()
+        const elementId = $(this).data('scroll')
+        const offset = $(elementId).offset()
+        $('html, body').animate({
+            scrollTop: offset.top - 40
+        }, 1000)
     })
 })
